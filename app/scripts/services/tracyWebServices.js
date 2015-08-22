a462dc3e9d1007fb64f61e0a14bce9a73a5c535a
Joao Vicente <vicentejo@dnb.com>
2015-08-22 11:51:42 +0100

    created layout for measure-task view

--- app/scripts/services/tracyWebServices.js
+++ app/scripts/services/tracyWebServices.js
@@ -7,6 +7,7 @@ var tracyWebServices =
 tracyWebServices.factory('TaskMeasurement', ['$resource',
 function($resource) {
 return $resource("http://localhost:8080/rest/tws/v1/applications/:application/tasks/:task/measurement", {}, {
+// return $resource("http://localhost:8080/rest/user/123", {}, {
   get: {method: 'GET', cache: false, isArray: false},	
   query: {method:'GET', isArray:true},
   save: {method: 'POST', cache: false, isArray: false},
