0b92b5957b92c6979edd48dd8f521cd285c5aefc
Joao Vicente <vicentejo@dnb.com>
2015-08-26 07:10:25 +0100

    Implemented GET /measurement periodical callout Issue outstanding: getChartData calling GET more than one time

--- app/scripts/controllers/measureTaskCtrl.js
+++ app/scripts/controllers/measureTaskCtrl.js
@@ -7,28 +7,33 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp', ['highcharts-ng', 'tracyWebServices', 'tracyChartService'])
-  .controller('MeasureTaskCtrl', ['$scope', '$stateParams', '$timeout', '$http', 'TaskMeasurement', 'tracyCharts',  
-  	function ($scope, $stateParams, $timeout, $http, TaskMeasurement, tracyCharts) {
+  .controller('MeasureTaskCtrl', ['$scope', '$stateParams', '$timeout', '$http', '$interval', 'TaskMeasurement', 'tracyCharts',  
+  	function ($scope, $stateParams, $timeout, $http, $interval, TaskMeasurement, tracyCharts) {
+	    $scope.application = $stateParams.application;
+	    $scope.task = $stateParams.task;
+	    $scope.measurement = null;
+	    $scope.refreshMsecPeriod = 10000;
 
-    $scope.application = $stateParams.application;
-    $scope.task = $stateParams.task;
-    $scope.measurement = null;
+	    $scope.getChartData = function () {
+		    	TaskMeasurement.get({application: "myApp", task: "myTask"},
+				function success(response) {
+				    $scope.measurement = response;
+				    // console.log($scope.measurement);
+				    // console.log("Success:" + JSON.stringify(response));
+				    console.log("GET /measurement")
 
-    TaskMeasurement.get({application: "myApp", task: "myTask"},
-		function success(response) {
-		    $scope.measurement = response;
-		    // console.log($scope.measurement);
-		    // console.log("Success:" + JSON.stringify(response));
-
-		    $scope.singleTaskApdexTimechart 
-    			= tracyCharts.getSingleTaskApdexTimechart($scope.application, $scope.task, $scope.measurement.dapdexTimechart);
-    		$scope.singleTaskVitalsTimechart = 
-    			tracyCharts.getSingleTaskVitalsTimechart($scope.application, $scope.task, $scope.measurement.vitalsTimechart);
-    		$scope.latencyHistogram = 
-    			tracyCharts.getLatencyHistogram($scope.application, $scope.task, $scope.measurement.latencyHistogram);
-		},
-		function error(errorResponse) {
-		    console.log("Error:" + JSON.stringify(errorResponse));
-		}
-	);
+				    $scope.singleTaskApdexTimechart 
+		    			= tracyCharts.getSingleTaskApdexTimechart($scope.application, $scope.task, $scope.measurement.dapdexTimechart);
+		    		$scope.singleTaskVitalsTimechart = 
+		    			tracyCharts.getSingleTaskVitalsTimechart($scope.application, $scope.task, $scope.measurement.vitalsTimechart);
+		    		$scope.latencyHistogram = 
+		    			tracyCharts.getLatencyHistogram($scope.application, $scope.task, $scope.measurement.latencyHistogram);
+				},
+				function error(errorResponse) {
+				    console.log("Error:" + JSON.stringify(errorResponse));
+				}
+			);
+	    };
+	    $scope.getChartData();
+	   	$interval(function(){ $scope.getChartData();}, $scope.refreshMsecPeriod);
 }]);
