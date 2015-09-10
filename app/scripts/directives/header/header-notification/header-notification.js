0c78e52a5b3a22d0586aadb77ff922fcbaf7c835
Joao Vicente <vicentejo@dnb.com>
2015-09-10 23:08:03 +0100

    Added UTC clock and environment placeholder

--- app/scripts/directives/header/header-notification/header-notification.js
+++ app/scripts/directives/header/header-notification/header-notification.js
@@ -6,13 +6,34 @@
  * @description
  * # adminPosHeader
  */
-angular.module('sbAdminApp')
-	.directive('headerNotification',function(){
-		return {
-        templateUrl:'scripts/directives/header/header-notification/header-notification.html',
-        restrict: 'E',
-        replace: true,
-    	}
-	});
+ angular.module('sbAdminApp')
+ .controller('HeaderNotificationCtrl', ['$scope', function($scope) {
+ 	$scope.clock = {
+ 		clock : new Date()
+ 	}
+ 	var updateClock = function () {
+ 		var m = new Date();
+ 		$scope.clock.now =
+ 		m.getUTCFullYear() +"/"+
+ 		("0" + (m.getUTCMonth()+1)).slice(-2) +"/"+
+ 		("0" + m.getUTCDate()).slice(-2) + " " +
+ 		("0" + m.getUTCHours()).slice(-2) + ":" +
+ 		("0" + m.getUTCMinutes()).slice(-2) + ":" +
+ 		("0" + m.getUTCSeconds()).slice(-2) + " UTC";
+ 	};
+
+ 	setInterval(function () {
+ 		$scope.$apply(updateClock);
+ 	}, 1000);
+
+ 	updateClock();
+ }])
+ .directive('headerNotification',function(){
+ 	return {
+ 		templateUrl:'scripts/directives/header/header-notification/header-notification.html',
+ 		restrict: 'E',
+ 		replace: true,
+ 	}
+ });
 
 
