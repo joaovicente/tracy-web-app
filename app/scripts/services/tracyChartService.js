dc0a42518ec53d4ccae8a5ccbeb241043e813423
Joao Vicente <vicentejo@dnb.com>
2015-10-30 21:27:28 +0000

    simulated batch app/task

--- app/scripts/services/tracyChartService.js
+++ app/scripts/services/tracyChartService.js
@@ -4,7 +4,7 @@
 var tracyChartService = 
  angular.module('tracyChartService', ['highcharts-ng']);
 
-
+var rttUnit = "?";
 var singleTaskApdexTimechart =             
         {
             options: {
@@ -117,9 +117,8 @@ var singleTaskApdexTimechart =
              height: 300
          }
      };
-
-     var singleTaskVitalsTemplate = 
-        {
+var singleTaskVitalsTemplate = 
+     {
             options: {
                 chart: {
                     zoomType: 'xy'
@@ -135,28 +134,32 @@ var singleTaskApdexTimechart =
             // subtitle: {
             //     text: 'Application/Task'
             // },
-                    xAxis: {
+            xAxis: {
                 type: 'datetime',
                 title: {
                     text: 'Time'
                 }
             },
             yAxis: [{ // Primary yAxis
+                visible: true,
                 labels: {
                     format: '{value}',
                     style: {
-                        color: Highcharts.getOptions().colors[2]
+                        // color: '#228B22'//Highcharts.getOptions().colors[2]
+                        color: Highcharts.getOptions().colors[7]
                     }
                 },
                 title: {
-                    text: 'Latency (p95)',
+                    text: 'Response time (ms)',
                     style: {
-                        color: Highcharts.getOptions().colors[2]
+                        // color: '#228B22' //Highcharts.getOptions().colors[2]
+                        color: Highcharts.getOptions().colors[7]
                     }
                 },
                 opposite: true
 
             }, { // Secondary yAxis
+                visible: true,
                 gridLineWidth: 0,
                 title: {
                     text: 'Count',
@@ -173,6 +176,7 @@ var singleTaskApdexTimechart =
                 min: 0
 
             }, { // Tertiary yAxis
+                visible: true,
                 gridLineWidth: 0,
                 title: {
                     text: 'Error count',
@@ -201,6 +205,7 @@ var singleTaskApdexTimechart =
             series: [{
                 name: 'Count',
                 type: 'column',
+                visible: true,
                 yAxis: 1,
                 data: [                
                     [1429975020000, 200   ],
@@ -223,6 +228,7 @@ var singleTaskApdexTimechart =
             }, {
                 name: 'Error count',
                 type: 'spline',
+                visible: true,
                 yAxis: 2,
                 data: [                                
                     [1429975020000, 1   ],
@@ -247,8 +253,35 @@ var singleTaskApdexTimechart =
                 },
                 dashStyle: 'shortdot'
             }, {
-                name: 'Latency (p95)',
+                name: 'p95 Response Time',
+                color: Highcharts.getOptions().colors[7],//'#228B22',
                 type: 'spline',
+                visible: true,
+                yAxis: 0,
+                data: [
+                    [1429975020000, 110   ],
+                    [1429975080000, 132   ],
+                    [1429975140000, 141   ],
+                    [1429975200000, 143   ],
+                    [1429975260000, 151   ],
+                    [1429975320000, 134   ],
+                    [1429975380000, 123   ],
+                    [1429975440000, 131   ],
+                    [1429975500000, 111   ],
+                    [1429975560000, 125   ],
+                    [1429975620000, 123   ],
+                    [1429975680000, 143   ],
+                    [1429975740000, 122   ],
+                    [1429975800000, 156   ],
+                    [1429975860000, 116   ],
+                    [1429975920000, 145   ]
+                    ],
+            }, {
+                name: 'max Response Time',
+                type: 'spline',
+                color: Highcharts.getOptions().colors[7],//'#228B22',
+                visible: true,
+                yAxis: 0,
                 data: [
                     [1429975020000, 110   ],
                     [1429975080000, 132   ],
@@ -387,164 +420,6 @@ var multiTaskApdexTimechart =
              height: 300
          }
      };
-
-     var singleTaskVitalsTemplate = 
-        {
-            options: {
-                chart: {
-                    zoomType: 'xy'
-                },
-                tooltip: {
-                    shared: true
-                }
-            },
-            title: {
-                text: ''
-                // text: 'Vitals'
-            },
-            // subtitle: {
-            //     text: 'Application/Task'
-            // },
-                    xAxis: {
-                type: 'datetime',
-                title: {
-                    text: 'Time'
-                }
-            },
-            yAxis: [{ // Primary yAxis
-                labels: {
-                    format: '{value}',
-                    style: {
-                        color: Highcharts.getOptions().colors[2]
-                    }
-                },
-                title: {
-                    text: 'Latency (p95)',
-                    style: {
-                        color: Highcharts.getOptions().colors[2]
-                    }
-                },
-                opposite: true
-
-            }, { // Secondary yAxis
-                gridLineWidth: 0,
-                title: {
-                    text: 'Count',
-                    style: {
-                        color: Highcharts.getOptions().colors[0]
-                    }
-                },
-                labels: {
-                    format: '{value}',
-                    style: {
-                        color: Highcharts.getOptions().colors[0]
-                    }
-                },
-                min: 0
-
-            }, { // Tertiary yAxis
-                gridLineWidth: 0,
-                title: {
-                    text: 'Error count',
-                    style: {
-                        color: Highcharts.getOptions().colors[1]
-                    }
-                },
-                labels: {
-                    format: '{value}',
-                    style: {
-                        color: Highcharts.getOptions().colors[1]
-                    }
-                },
-                opposite: true,
-                min: 0
-            }],
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                x: 80,
-                verticalAlign: 'top',
-                y: 55,
-                floating: true,
-                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-            },
-            series: [{
-                name: 'Count',
-                type: 'column',
-                yAxis: 1,
-                data: [                
-                    [1429975020000, 200   ],
-                    [1429975080000, 243   ],
-                    [1429975140000, 254   ],
-                    [1429975200000, 234   ],
-                    [1429975260000, 253   ],
-                    [1429975320000, 265   ],
-                    [1429975380000, 245   ],
-                    [1429975440000, 247   ],
-                    [1429975500000, 765   ],
-                    [1429975560000, 243   ],
-                    [1429975620000, 265   ],
-                    [1429975680000, 273   ],
-                    [1429975740000, 247   ],
-                    [1429975800000, 256   ],
-                    [1429975860000, 236   ],
-                    [1429975920000, 245   ]
-                ],
-            }, {
-                name: 'Error count',
-                type: 'spline',
-                yAxis: 2,
-                data: [                                
-                    [1429975020000, 1   ],
-                    [1429975080000, 2   ],
-                    [1429975140000, 1   ],
-                    [1429975200000, 2   ],
-                    [1429975260000, 1   ],
-                    [1429975320000, 2   ],
-                    [1429975380000, 1   ],
-                    [1429975440000, 2   ],
-                    [1429975500000, 4   ],
-                    [1429975560000, 1   ],
-                    [1429975620000, 2   ],
-                    [1429975680000, 1   ],
-                    [1429975740000, 2   ],
-                    [1429975800000, 1   ],
-                    [1429975860000, 2   ],
-                    [1429975920000, 1   ]
-                    ],
-                marker: {
-                    enabled: false
-                },
-                dashStyle: 'shortdot'
-            }, {
-                name: 'Latency (p95)',
-                type: 'spline',
-                data: [
-                    [1429975020000, 110   ],
-                    [1429975080000, 132   ],
-                    [1429975140000, 141   ],
-                    [1429975200000, 143   ],
-                    [1429975260000, 151   ],
-                    [1429975320000, 134   ],
-                    [1429975380000, 123   ],
-                    [1429975440000, 131   ],
-                    [1429975500000, 111   ],
-                    [1429975560000, 125   ],
-                    [1429975620000, 123   ],
-                    [1429975680000, 143   ],
-                    [1429975740000, 122   ],
-                    [1429975800000, 156   ],
-                    [1429975860000, 116   ],
-                    [1429975920000, 145   ]
-                    ],
-            }],
-            credits: {
-                enabled: false
-            },
-            size: {
-             height: 300
-         }
-        };
 var singleTaskHistogramTemplate = 
        {
             options : { 
@@ -579,7 +454,7 @@ var singleTaskHistogramTemplate =
                     '0-40'
                 ],
                 title: {
-                    text: 'latency range [ms]',
+                    text: 'Response time range [ms]',
                 }
             },
             yAxis: {
@@ -603,7 +478,7 @@ var singleTaskHistogramTemplate =
                 enabled: false
             },
             series: [{
-                name: 'Count within latency range',
+                name: 'Count within response time range',
                 data: [
                     {y: 3, color: '#C92524'},
                     {y: 4, color: '#D0FEC0'},
@@ -635,6 +510,7 @@ tracyChartService.factory('tracyCharts', function(){
 	        	}
 			}
 			singleTaskApdexTimechart.series[0].data = newData;
+            rttUnit = chartData.rttUnit;
             singleTaskApdexTimechart.series[0].name = 'APDEX [' + chartData.rttT + chartData.rttUnit + ']';
 			// console.log(singleTaskApdexTimechart);
             return singleTaskApdexTimechart;
@@ -678,21 +554,35 @@ tracyChartService.factory('tracyCharts', function(){
 
         getSingleTaskVitalsTimechart: function(application, task, chartData){
         	// console.log(chartData);
-            var i = 0, countData = [], errorCountData = [], p95Data = [];
+            var i = 0, countData = [], errorCountData = [], p95Data = [], maxData = [];
+            var totalErrorCount=0, maxSnapCount=0;
 			for (i = 0; i < chartData.timeSequence.length; i++) {
 				if (null != chartData.count[i])	{
 	            	countData.push([chartData.timeSequence[i], chartData.count[i]]);
+                    maxSnapCount = Math.max(maxSnapCount, chartData.count[i]);
 	        	}
 				if (null != chartData.errors[i])	{
 	            	errorCountData.push([chartData.timeSequence[i], chartData.errors[i]]);
+                    totalErrorCount = totalErrorCount + chartData.errors[i];
 	        	}
 				if (null != chartData.p95[i])	{
 	            	p95Data.push([chartData.timeSequence[i], chartData.p95[i]]);
 	        	}
+                if (null != chartData.max[i])   {
+                    maxData.push([chartData.timeSequence[i], chartData.max[i]]);
+                }
 			}
+            // console.log(singleTaskVitalsTemplate);
+            singleTaskVitalsTemplate.yAxis[0].title.text = 'Response time (' + rttUnit+ ')',
 			singleTaskVitalsTemplate.series[0].data = countData;
+            singleTaskVitalsTemplate.series[0].visible = (maxSnapCount == 1 ?  false : true);
 			singleTaskVitalsTemplate.series[1].data = errorCountData;
+            singleTaskVitalsTemplate.series[1].visible = (totalErrorCount == 0 ?  false : true);
 			singleTaskVitalsTemplate.series[2].data = p95Data;
+            singleTaskVitalsTemplate.series[2].visible = (maxSnapCount == 1 ?  false : true);
+            singleTaskVitalsTemplate.series[3].data = maxData;
+            singleTaskVitalsTemplate.series[3].visible = (maxSnapCount == 1 ?  true : false);
+
 			// console.log(singleTaskVitalsTemplate);
 			// console.log(JSON.stringify(singleTaskVitalsTemplate));
             return singleTaskVitalsTemplate;
@@ -724,6 +614,7 @@ tracyChartService.factory('tracyCharts', function(){
 	        	}
 			}
 			singleTaskHistogramTemplate.xAxis.categories = binsData;
+            singleTaskHistogramTemplate.xAxis.title.text = 'Response time range [' + rttUnit + ']',
 			singleTaskHistogramTemplate.series[0].data = countAndColourData;
 			// console.log(singleTaskHistogramTemplate);
 			// console.log(JSON.stringify(singleTaskHistogramTemplate));
