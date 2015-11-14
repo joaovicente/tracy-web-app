4ec09a6b2d5c9de9edc5a0a5cb14935bd72de660
Joao Vicente <vicentejo@dnb.com>
2015-11-14 09:23:27 +0000

    tracy-timeline-viewer mock tracyTasks REST backend in TWA

--- app/scripts/controllers/timelineCtrl.js
+++ app/scripts/controllers/timelineCtrl.js
@@ -30,7 +30,7 @@ app.controller('TimelineCtrl', ['$scope', '$stateParams','tracyTaskGraph', funct
 
   function nextUrl()  {
     var increment = 1;
-    if ($scope.sequenceId == 20) {
+    if ($scope.sequenceId == $scope.lastId) {
       increment = 0;
     }
     var nextUrl = "/timeline/" 
@@ -46,11 +46,69 @@ app.controller('TimelineCtrl', ['$scope', '$stateParams','tracyTaskGraph', funct
 
   function disableIfLast() {
     var disabled = "";
-    if ($scope.sequenceId == 20) {
+    if ($scope.sequenceId == $scope.lastId) {
       disabled = "disabled";
     }
+    // console.log($scope.sequenceId + "-" +$scope.lastId);
     return disabled;
   }
+
+  function mockTracyEvent(timeOffset)  {
+    var rt = 1446415872559;
+    // var offset = 10; // msecOffset
+    // var offset = 1010; // secOffset
+    // var offset = 61010; // minOffset
+    var offset = 3601000; // hourOffset
+    var tracyEvents = [
+      {"taskId":"TID-ab1234-x","parentOptId":"4F3D","label":"foo","optId":"AD24","msecBefore":timeOffset+rt+offset*5,"msecAfter":timeOffset+rt+offset*7,"msecElapsed":offset*2,"host":"ukdb807735-3.local","component":"Service"}
+      ,{"taskId":"TID-ab1234-x","parentOptId":"4F3D","label":"bar","optId":"AE5F","msecBefore":timeOffset+rt+offset*3,"msecAfter":timeOffset+rt+offset*5,"msecElapsed":offset*2,"host":"ukdb807735-3.local","component":"Service"}
+      ,{"taskId":"TID-ab1234-x","parentOptId":"23CF","label":"Http servlet","optId":"4F3D","msecBefore":timeOffset+rt+offset*2,"msecAfter":timeOffset+rt+offset*8,"msecElapsed":offset*6,"host":"ukdb807735-3.local","component":"Service"}
+      ,{"taskId":"TID-ab1234-x","parentOptId":"DBF5","label":"Service handler","optId":"23CF","msecBefore":timeOffset+rt+offset,"msecAfter":timeOffset+rt+offset*9,"msecElapsed":offset*8,"host":"ukdb807735-3.local","component":"Proxy"}
+      ,{"taskId":"TID-ab1234-x","parentOptId":"AAAA","label":"Client handler","optId":"DBF5","msecBefore":timeOffset+rt,"msecAfter":timeOffset+rt+offset*10,"msecElapsed":offset*10,"host":"ukdb807735-3.local","component":"Proxy"}
+      ];
+    return tracyEvents;
+  }
+
+  function mockTracyTaskCreation(timeOffset)  {
+    var tracyTask = {tracyTask: {tracyEvents: []}};
+    tracyTask.tracyTask.tracyEvents = mockTracyEvent(timeOffset);
+    return tracyTask;
+  }
+
+  //TaskMeasurement.get({application: $scope.application, task: $scope.task},
+  function mockTaskAnalysisReource(query) {
+    // GET .../tws/v1/applications/Direct+/tasks/IDR-Golf/analysis
+    // &filter=msecBefore:[1446187500000 TO 1446201000000] AND msecElapsed:[120 TO 180] AND t_application:”Direct+" AND t_task:”IDR-Golf"
+    // ?sort=-msecElapsed
+    // &offset=0
+    // &limit=20
+    var response = {application: '', task: '', tracyTasksPage : {}};
+    response.application = query.application;
+    response.task = query.task;
+    response.tracyTasksPage.offset = 0;
+    response.tracyTasksPage.limit = 20;
+    response.tracyTasksPage.records = 18;
+    response.tracyTasksPage.tracyTasks = [];
+    for (var i=0 ; i<response.tracyTasksPage.records ; i++) {
+      response.tracyTasksPage.tracyTasks.push(mockTracyTaskCreation(3600000*i));
+    }
+    // console.log(response);
+    return response;
+  }
+
+  function prepareAnalysisQuery(application, task, earliest, latest, rtAbove, rtBelow) {
+    var query = {};
+    query.application = application;
+    query.task = task;
+    query.filter = "msecAfter:[" + earliest + " TO " + latest + "]"
+      + " AND msecElapsed:[" + rtAbove + "TO" + rtBelow + "]";
+    query.sort = "-msecElapsed";
+    query.offset = "0";
+    query.limit = "20";
+    // console.log(query);
+    return query;
+  }
+  // TODO: tracy-timeline-viewer controller: maxSequenceNumber, timelineArray, tracyTaskArray, rawTwsResponsesMap
 
   $scope.chart = {};
   var rt = 1446415872559;
@@ -59,7 +117,7 @@ app.controller('TimelineCtrl', ['$scope', '$stateParams','tracyTaskGraph', funct
     // var offset = 61010; // minOffset
   var offset = 3601000; // hourOffset
 
-    // TODO: Make earliest and latest human readable as per GMT clock
+
   $scope.earliest = new Date(Number($stateParams['earliest']));
   $scope.latest = new Date(Number($stateParams['latest']));
   $scope.rtAbove = $stateParams['rtAbove'];
@@ -67,24 +125,33 @@ app.controller('TimelineCtrl', ['$scope', '$stateParams','tracyTaskGraph', funct
   $scope.sequenceId = $stateParams['sequenceId'];
   $scope.application = (typeof $stateParams['application'] === 'undefined') ? 'unknown-application' : $stateParams['application'];
   $scope.task = (typeof $stateParams['task'] === 'undefined') ? 'unknown-task' : $stateParams['task'];
-  $scope.disablePrevious = disableIfFirst();
-  $scope.disableLast = disableIfLast();
-  $scope.prevUrl = previousUrl();
-  $scope.nextUrl = nextUrl();
+
+  var query = prepareAnalysisQuery($scope.application, $scope.task, $stateParams['earliest'], $stateParams['latest'], $scope.rtAbove, $scope.rtBelow);
+  var taskAnalysisResponse = mockTaskAnalysisReource(query);
+
+  $scope.lastId = taskAnalysisResponse.tracyTasksPage.records;
 
   // Object.keys($stateParams).forEach(function(key) {
   //  console.log(key, $stateParams[key]);
   // });
 
-  $scope.tracyTask = [
-    {"taskId":"TID-ab1234-x","parentOptId":"4F3D","label":"foo","optId":"AD24","msecBefore":rt+offset*5,"msecAfter":rt+offset*7,"msecElapsed":offset*2,"host":"ukdb807735-3.local","component":"Service"}
-    ,{"taskId":"TID-ab1234-x","parentOptId":"4F3D","label":"bar","optId":"AE5F","msecBefore":rt+offset*3,"msecAfter":rt+offset*5,"msecElapsed":offset*2,"host":"ukdb807735-3.local","component":"Service"}
-    ,{"taskId":"TID-ab1234-x","parentOptId":"23CF","label":"Http servlet","optId":"4F3D","msecBefore":rt+offset*2,"msecAfter":rt+offset*8,"msecElapsed":offset*6,"host":"ukdb807735-3.local","component":"Service"}
-    ,{"taskId":"TID-ab1234-x","parentOptId":"DBF5","label":"Service handler","optId":"23CF","msecBefore":rt+offset,"msecAfter":rt+offset*9,"msecElapsed":offset*8,"host":"ukdb807735-3.local","component":"Proxy"}
-    ,{"taskId":"TID-ab1234-x","parentOptId":"AAAA","label":"Client handler","optId":"DBF5","msecBefore":rt,"msecAfter":rt+offset*10,"msecElapsed":offset*10,"host":"ukdb807735-3.local","component":"Proxy"}
-  ];
+  // $scope.tracyTask = [
+  //   {"taskId":"TID-ab1234-x","parentOptId":"4F3D","label":"foo","optId":"AD24","msecBefore":rt+offset*5,"msecAfter":rt+offset*7,"msecElapsed":offset*2,"host":"ukdb807735-3.local","component":"Service"}
+  //   ,{"taskId":"TID-ab1234-x","parentOptId":"4F3D","label":"bar","optId":"AE5F","msecBefore":rt+offset*3,"msecAfter":rt+offset*5,"msecElapsed":offset*2,"host":"ukdb807735-3.local","component":"Service"}
+  //   ,{"taskId":"TID-ab1234-x","parentOptId":"23CF","label":"Http servlet","optId":"4F3D","msecBefore":rt+offset*2,"msecAfter":rt+offset*8,"msecElapsed":offset*6,"host":"ukdb807735-3.local","component":"Service"}
+  //   ,{"taskId":"TID-ab1234-x","parentOptId":"DBF5","label":"Service handler","optId":"23CF","msecBefore":rt+offset,"msecAfter":rt+offset*9,"msecElapsed":offset*8,"host":"ukdb807735-3.local","component":"Proxy"}
+  //   ,{"taskId":"TID-ab1234-x","parentOptId":"AAAA","label":"Client handler","optId":"DBF5","msecBefore":rt,"msecAfter":rt+offset*10,"msecElapsed":offset*10,"host":"ukdb807735-3.local","component":"Proxy"}
+  // ];
 
-  tracyTaskGraph.addTracyTask($scope.tracyTask);
+  // tracyTaskGraph.addTracyTask($scope.tracyTask);
+  // console.log($scope.tracyTask);
+  // console.log(taskAnalysisResponse.tracyTasksPage.tracyTasks[$scope.sequenceId-1].tracyTask.tracyEvents);
+  tracyTaskGraph.addTracyTask(taskAnalysisResponse.tracyTasksPage.tracyTasks[$scope.sequenceId-1].tracyTask.tracyEvents);
   $scope.chart = tracyTaskGraph.asGoogleTimeline(0);
   // console.log(tracyTaskGraph.inspect());
+
+  $scope.disablePrevious = disableIfFirst();
+  $scope.disableLast = disableIfLast();
+  $scope.prevUrl = previousUrl();
+  $scope.nextUrl = nextUrl();
 }]);
